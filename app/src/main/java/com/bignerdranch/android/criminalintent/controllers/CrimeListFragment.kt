d80855890740244f3a3efcae36ebac4a09a8e341
Agustina <agustina@xmartlabs.com>
2019-12-05 16:04:23 -0300

    chapter 11 created

--- app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeListFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.CrimeAdapter
 import com.bignerdranch.android.criminalintent.R
+import com.bignerdranch.android.criminalintent.models.Crime
 import com.bignerdranch.android.criminalintent.models.CrimeListViewModel
 
 private const val TAG = "CrimeListFragment"
@@ -19,27 +21,30 @@ class CrimeListFragment : Fragment(R.layout.fragment_crime_list) {
   }
 
   private lateinit var crimeRecyclerView: RecyclerView
-  private var adapter: CrimeAdapter? = null
+  private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
   private val crimeListViewModel: CrimeListViewModel by lazy {
     ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
   }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-  }
-
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    crimeListViewModel.crimeListLiveData.observe(
+      viewLifecycleOwner,
+      Observer { crimes ->
+        crimes?.let {
+          Log.i(TAG, "Got crimes ${crimes.size}")
+          updateUI(crimes)
+        }
+      }
+    )
+
     crimeRecyclerView = view.findViewById(R.id.crimeRecyclerView) as RecyclerView
     crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-    updateUI()
+    crimeRecyclerView.adapter = adapter
   }
 
-  private fun updateUI() {
-    val crimes = crimeListViewModel.crimes
+  private fun updateUI(crimes: List<Crime>) {
     adapter = CrimeAdapter(crimes)
     crimeRecyclerView.adapter = adapter
   }
--- app/src/main/java/com/bignerdranch/android/criminalintent/models/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/models/CrimeListViewModel.kt
@@ -1,24 +1,9 @@
 package com.bignerdranch.android.criminalintent.models
 
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.criminalintent.CrimeRepository
 
 class CrimeListViewModel : ViewModel() {
-  companion object {
-    const val LIST_LENGHT = 100
-  }
-
-  val crimes: MutableList<Crime> = mutableListOf()
-
-  init {
-    mockData()
-  }
-
-  private fun mockData() {
-    for (i in 0 until LIST_LENGHT) {
-      val crime = Crime()
-      crime.title = "Crime #$i"
-      crime.isSolved = i % 2 == 0
-      crimes += crime
-    }
-  }
+  private val crimeRepository = CrimeRepository.get()
+  val crimeListLiveData = crimeRepository.getCrimes()
 }
