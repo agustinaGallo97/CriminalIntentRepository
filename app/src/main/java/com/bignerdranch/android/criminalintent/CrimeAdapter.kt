381b791246c0275ea83abdd192aa11963335452b
Agustina <33355750+agustinaGallo97@users.noreply.github.com>
2019-12-05 11:38:56 -0300

    chapter 10 created (#3)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeAdapter.kt
@@ -1,12 +1,16 @@
 package com.bignerdranch.android.criminalintent
 
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.models.Crime
+import com.bignerdranch.android.criminalintent.views.utils.context
 
 class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeAdapter.CrimeHolder>() {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
@@ -25,16 +29,17 @@ class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeAdapter.
 
     private val titleTextView: TextView = itemView.findViewById(R.id.crimeTitle)
     private val dateTextView: TextView = itemView.findViewById(R.id.crimeDate)
+    private val solvedImageView: ImageView = itemView.findViewById(R.id.crimeSolved)
 
     fun bind(crime: Crime) {
       titleTextView.text = crime.title
-      dateTextView.text = crime.date.toString()
-      with(itemView) {
-        var text = context.getString(R.string.crime_title_pressed, crime.title)
-        setOnClickListener {
-          Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
-        }
+      dateTextView.text =
+        DateUtils.formatDateTime(context, crime.date.time, DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_YEAR)
+      itemView.setOnClickListener {
+        val text = context.getString(R.string.crime_title_pressed, crime.title)
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
       }
+      solvedImageView.isVisible = crime.isSolved
     }
   }
 }
