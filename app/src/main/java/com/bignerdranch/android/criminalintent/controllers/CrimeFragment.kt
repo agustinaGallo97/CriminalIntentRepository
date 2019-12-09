b623a91382a92b8675669c99b3b6948f8551f085
Agustina <33355750+agustinaGallo97@users.noreply.github.com>
2019-12-09 09:40:47 -0300

    Update app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeFragment.kt
    Co-Authored-By: Bruno Ferrari <bruno@xmartlabs.com>

--- app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeFragment.kt
@@ -17,6 +17,7 @@ import java.util.UUID
 class CrimeFragment : Fragment(R.layout.fragment_crime) {
   companion object {
     private const val ARG_CRIME_ID = "crime_id"
+
     fun newInstance(crimeId: UUID): CrimeFragment {
       val args = Bundle().apply {
         putSerializable(ARG_CRIME_ID, crimeId)
